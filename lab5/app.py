1bb490077bf27b344e70bc37d04f2f120938ae9b
Coroletchi Ana <anacoroletchi@yahoo.com>
2023-06-05 15:27:52 +0300

    lab5 code modifications

--- lab5/app.py
+++ lab5/app.py
@@ -1,18 +1,38 @@
 import telebot
+from flask import Flask, request
+import os
 
 bot = telebot.TeleBot("6265105993:AAH37Vy19xNAs6k2ZjdAkGIwTFOSBAmDoSA")
+server = Flask(__name__)
+
+TOKEN = "6265105993:AAH37Vy19xNAs6k2ZjdAkGIwTFOSBAmDoSA"
+WEBHOOK_URL = "https://covrig.fly.dev"
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-	bot.reply_to(message, "Say buna zîua to covrig_bot. Do u want some fake news? Here we go, fake covrigi")
-	
+    bot.reply_to(message, "Say buna zîua to covrig_bot. Do u want some fake news? Here we go, fake covrigi")
+    
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-	bot.reply_to(message, "Pentru ajutor, sunați la 112")
+    bot.reply_to(message, "Pentru ajutor, sunați la 112")
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)
 
-bot.infinity_polling()
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=WEBHOOK_URL + "/" + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
